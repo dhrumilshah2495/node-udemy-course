597757f96c0ca30ff3065b958d670d2ffd35ed86
Dhwani shah <Dhwani shah>
2017-02-21 15:43:49 +0530

    'changes'

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,6 +1,6 @@
 var mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;//we are configuring mongoose to use inbuild promise
-mongoose.connect('mongodb://localhost:27017/ToDoApp');//creating moongose connection
-
+mongoose.connect('mongodb://admin:root@ds157459.mlab.com:57459/mlabdatabase');//creating moongose connection
+//mongodb://localhost:27017/ToDoApp
 module.exports = {mongoose};
