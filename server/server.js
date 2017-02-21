06ecabb372f5609a7f2b9ca7700f68f040e4bd33
Dhwani shah <Dhwani shah>
2017-02-21 14:36:00 +0530

    'changes'

--- server/server.js
+++ server/server.js
@@ -2,37 +2,41 @@ var express = require('express');
 var bodyParser = require('body-parser');
 var {ObjectID} = require('mongodb');
 var {mongoose} = require('./db/mongoose');
-var {ToDo} = require('./models/toDo');
-var  {User} = require('./models/user');
+// var {ToDo} = require('./models/toDo');
+// var  {User} = require('./models/user');
 
 var app = express();
 
-app.use(bodyParser.json());
-
-app.set('port',process.env.PORT || 3000);
-
-app.post('/todos',(req,res)=>{
-  var todo = new ToDo({text:req.body.text});
-  todo.save().then((doc)=>{
-    res.send(doc);
-  },(e)=>{
-    res.status(400).send(e);
-  });
+app.get('/',(req,res)=>{
+  res.send('Welcome To My App....');
 });
 
-app.get('/todos/:id',(req,res)=>{
-  var id = req.params.id;
-  if(!ObjectID.isValid(id)){
-    return res.status(404).send();
-  }
-  ToDo.findById(id).then((todo)=>{
-    if(!todo){
-      return res.status(404).send();
-    }
-    res.send({todo});
-  });
-  res.send(req.params);
-});
+// app.use(bodyParser.json());
+//
+// app.set('port',process.env.PORT || 3000);
+//
+// app.post('/todos',(req,res)=>{
+//   var todo = new ToDo({text:req.body.text});
+//   todo.save().then((doc)=>{
+//     res.send(doc);
+//   },(e)=>{
+//     res.status(400).send(e);
+//   });
+// });
+//
+// app.get('/todos/:id',(req,res)=>{
+//   var id = req.params.id;
+//   if(!ObjectID.isValid(id)){
+//     return res.status(404).send();
+//   }
+//   ToDo.findById(id).then((todo)=>{
+//     if(!todo){
+//       return res.status(404).send();
+//     }
+//     res.send({todo});
+//   });
+//   res.send(req.params);
+// });
 
 app.listen(app.get('port'),()=>{
   console.log('Started on :'+app.get('port'));
